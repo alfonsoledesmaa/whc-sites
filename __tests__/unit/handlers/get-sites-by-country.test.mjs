5bb6d57fa86433d6f81af60e7aa40528c112f5ac
Alfonso Ledesma <alfonsoledesmaa@gmail.com>
2024-11-15 18:27:19 -0600

    Add tests for Get Sites by Country

--- __tests__/unit/handlers/get-sites-by-country.test.mjs
+++ __tests__/unit/handlers/get-sites-by-country.test.mjs
@@ -1,43 +1,120 @@
-// Import getByIdHandler function from get-by-id.mjs 
-import { getByIdHandler } from '../../../src/handlers/get-by-id.mjs'; 
-// Import dynamodb from aws-sdk 
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
-import { mockClient } from "aws-sdk-client-mock";
- 
-// This includes all tests for getByIdHandler() 
-describe('Test getByIdHandler', () => { 
-    const ddbMock = mockClient(DynamoDBDocumentClient);
- 
-    beforeEach(() => {
-        ddbMock.reset();
-      });
- 
-    // This test invokes getByIdHandler() and compare the result  
-    it('should get item by id', async () => { 
-        const item = { id: 'id1' }; 
- 
-        // Return the specified value whenever the spied get function is called 
-        ddbMock.on(GetCommand).resolves({
-            Item: item,
-        }); 
- 
+// Import getAllItemsHandler function from get-all-items.mjs 
+import { getSitesSummaryByCountryHandler } from '../../../src/handlers/get-sites-by-country.mjs';
+
+// This includes all tests for getAllItemsHandler() 
+describe('Test getSitesByLanguageHandler', () => {
+    const result = {
+        country: "Austria",
+        total_sites: 7,
+        site_counts: {
+            cultural: 7,
+            natural: 0,
+            mixed: 0
+        },
+        sites: [
+            {
+                id: "784",
+                name: "Historic Centre of the City of Salzburg",
+                region: "Europe and North America"
+            },
+            {
+                id: "785",
+                name: "Semmering Railway",
+                region: "Europe and North America"
+            },
+            {
+                id: "786",
+                name: "Palace and Gardens of Schönbrunn",
+                region: "Europe and North America"
+            },
+            {
+                id: "806",
+                name: "Hallstatt-Dachstein / Salzkammergut Cultural Landscape",
+                region: "Europe and North America"
+            },
+            {
+                id: "931",
+                name: "City of Graz – Historic Centre and Schloss Eggenberg",
+                region: "Europe and North America"
+            },
+            {
+                id: "970",
+                name: "Wachau Cultural Landscape",
+                region: "Europe and North America"
+            },
+            {
+                id: "1033",
+                name: "Historic Centre of Vienna",
+                region: "Europe and North America"
+            }
+        ]
+    };
+
+    it('should return seven spots from Austria', async () => { 
+        const data = {
+            country: "Austria",
+            total_sites: 7,
+            site_counts: {
+                cultural: 7,
+                natural: 0,
+                mixed: 0
+            },
+            sites: [
+                {
+                    id: "784",
+                    name: "Historic Centre of the City of Salzburg",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "785",
+                    name: "Semmering Railway",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "786",
+                    name: "Palace and Gardens of Schönbrunn",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "806",
+                    name: "Hallstatt-Dachstein / Salzkammergut Cultural Landscape",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "931",
+                    name: "City of Graz – Historic Centre and Schloss Eggenberg",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "970",
+                    name: "Wachau Cultural Landscape",
+                    region: "Europe and North America"
+                },
+                {
+                    id: "1033",
+                    name: "Historic Centre of Vienna",
+                    region: "Europe and North America"
+                }
+            ]
+        };
+        
+
         const event = { 
-            httpMethod: 'GET', 
-            pathParameters: { 
-                id: 'id1' 
-            } 
+            httpMethod: 'GET',
+            pathParameters: {
+                country_name: "Austria"
+            }
         };
  
-        // Invoke getByIdHandler() 
-        const result = await getByIdHandler(event); 
+        // Invoke helloFromLambdaHandler() 
+        const result = await getSitesSummaryByCountryHandler(event); 
  
         const expectedResult = { 
             statusCode: 200, 
-            body: JSON.stringify(item) 
+            body: JSON.stringify(data) 
         }; 
  
         // Compare the result with the expected result 
         expect(result).toEqual(expectedResult); 
     }); 
 }); 
- 
